05a55c6c3ff53e6e885b42e71f2388b433b8a081
Your New Username <yournewemail@example.com>
2025-09-28 01:31:45 +0500

    fix: subcription entity

--- internal/models/entities/subcription.go
+++ internal/models/entities/subcription.go
@@ -1,16 +1,15 @@
 package entities
 
 import (
-	"database/sql"
 	"time"
 )
 
 type Subscription struct {
-	ID             int64         `db:"id"`
-	Name           string        `db:"name"`
-	Price          int64         `db:"price"`
-	DurationDays   int64         `db:"duration_days"`
-	DailyLimitCups sql.NullInt32 `db:"daily_limit_cups"`
-	TotalCups      sql.NullInt32 `db:"total_cups"`
-	CreatedAt      time.Time     `db:"created_at"`
+	ID             int64     `db:"id"`
+	Name           string    `db:"name"`
+	Price          int64     `db:"price"`
+	DurationDays   int64     `db:"duration_days"`
+	DailyLimitCups int64     `db:"daily_limit_cups"`
+	TotalCups      int64     `db:"total_cups"`
+	CreatedAt      time.Time `db:"created_at"`
 }
