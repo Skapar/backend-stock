f5932747824bea6b47d98f0ca3a84087729f1aec
Your New Username <yournewemail@example.com>
2025-09-28 05:20:17 +0500

    format: text const

---
+++ internal/bot/const.go
@@ -0,0 +1,22 @@
+package bot
+
+// Button Names
+const (
+	STARTBUTTONNAME = "Start"
+)
+
+// Pressed Button Texts
+const (
+	UNEXISTINGBUTTONPRESSED = "Неизвестная команда. Пожалуйста, используйте доступные кнопки."
+)
+
+const (
+	STARTGREETINGTEXT    = "Добро пожаловать в OneCoffee! Пожалуйста, отправьте ваше имя и номер телефона."
+	SENDCONTACTTEXT      = "Отправить cвой контакт"
+	THANKYOUREGISTERTEXT = "Спасибо, %s! Вы успешно зарегистрированы в OneCoffee."
+)
+
+// Error texts
+const (
+	ERRORREGISTRATIONTEXT = "Произошла ошибка при регистрации. Пожалуйста, попробуйте еще раз."
+)
--- internal/bot/handlers.go
+++ internal/bot/handlers.go
@@ -24,19 +24,16 @@ func (b *telegramBot) handleUpdate(update tgbotapi.Update) {
 		err := b.handleRegisterUser(tgUserID, username, name, phone)
 		if err != nil {
 			b.log.Error(err)
-			b.reply(chatID, "Произошла ошибка при регистрации. Пожалуйста, попробуйте еще раз.")
 			return
 		}
-
-		b.reply(chatID, "Спасибо! Мы получили ваш номер: "+phone+" (Имя: "+name+")")
 	}
 
 	if update.Message.IsCommand() {
 		switch update.Message.Command() {
-		case "start":
+		case STARTBUTTONNAME:
 			b.handleStart(update)
 		default:
-			b.reply(chatID, "Неизвестная команда")
+			b.reply(chatID, UNEXISTINGBUTTONPRESSED)
 		}
 	}
 }
@@ -44,18 +41,14 @@ func (b *telegramBot) handleUpdate(update tgbotapi.Update) {
 func (b *telegramBot) handleStart(update tgbotapi.Update) {
 	chatID := update.Message.Chat.ID
 
-	// Текст приветствия
-	text := "Добро пожаловать в OneCoffee! Пожалуйста, отправьте ваше имя и номер телефона."
-
-	// Кнопка для отправки контакта
-	contactButton := tgbotapi.NewKeyboardButtonContact("Отправить свой номер")
+	contactButton := tgbotapi.NewKeyboardButtonContact(SENDCONTACTTEXT)
 	keyboard := tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(contactButton),
 	)
 	keyboard.OneTimeKeyboard = true
 	keyboard.ResizeKeyboard = true
 
-	msg := tgbotapi.NewMessage(chatID, text)
+	msg := tgbotapi.NewMessage(chatID, STARTGREETINGTEXT)
 	msg.ReplyMarkup = keyboard
 
 	_, err := b.tg.Send(msg)
@@ -74,11 +67,11 @@ func (b *telegramBot) handleRegisterUser(tgUserID int64, username, name, phone s
 
 	err := b.service.CreateOrUpdateUser(context.Background(), user)
 	if err != nil {
-		b.reply(tgUserID, "Произошла ошибка при регистрации, попробуйте позже.")
+		b.reply(tgUserID, ERRORREGISTRATIONTEXT)
 		b.log.Errorf("failed to register user: %v", err)
 		return err
 	}
 
-	b.reply(tgUserID, fmt.Sprintf("Спасибо, %s! Вы успешно зарегистрированы в OneCoffee.", name))
+	b.reply(tgUserID, fmt.Sprintf(THANKYOUREGISTERTEXT, name))
 	return nil
 }
