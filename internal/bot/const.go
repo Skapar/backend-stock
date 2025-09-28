c7ad0aa2449c98b49127a104a68355bab06bf451
Your New Username <yournewemail@example.com>
2025-09-28 05:49:39 +0500

    format: const text keyboard buttons

--- internal/bot/const.go
+++ internal/bot/const.go
@@ -10,12 +10,9 @@ const (
 // Pressed Button Texts
 const (
 	UNEXISTINGBUTTONPRESSED = "Неизвестная команда. Пожалуйста, используйте доступные кнопки."
-)
-
-const (
-	STARTGREETINGTEXT    = "Добро пожаловать в OneCoffee! Пожалуйста, отправьте ваше имя и номер телефона."
-	SENDCONTACTTEXT      = "Отправить cвой контакт"
-	THANKYOUREGISTERTEXT = "Спасибо, %s! Вы успешно зарегистрированы в OneCoffee."
+	STARTGREETINGTEXT       = "Добро пожаловать в OneCoffee! Пожалуйста, отправьте ваше имя и номер телефона."
+	SENDCONTACTTEXT         = "Отправить cвой контакт"
+	THANKYOUREGISTERTEXT    = "Спасибо, %s! Вы успешно зарегистрированы в OneCoffee."
 )
 
 // Error texts
